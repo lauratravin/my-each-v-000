e39383776fb40ba9a21e3baa921014539b75d81a
Laura Messa Travin <laura.travin@gmail.com>
2018-04-08 02:01:04 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+   i = 0
+   if block_given?
+         while i < array.length
+            yield (array[i])
+             i += 1
+         end
+    else
+        puts "Hey! No block was given!"
+    end
+   return array
 end
+
+collection = [1, 2, 3, 4]
+
+my_each(collection) { |value|
+  puts value }
